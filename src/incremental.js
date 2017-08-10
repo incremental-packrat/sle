16ae16e7f668f7f0b53e10a39a2e9b250ec76428
Anonymous Author(s) <anonymous@example.com>
2017-08-10 10:33:21 -0400

    Fix up some lint errors.

--- src/incremental.js
+++ src/incremental.js
@@ -1,146 +1,141 @@
 'use strict';
 
-/*
-  Boilerplate for "universal module definition" (UMD).
-  In Node.js, you can `require` this file to get a single object containing all the exports.
-  In the browser, use `<script src="...">` and the exports will be assigned into `window`.
- */
-(function(g,f){if(typeof define==='function'&&define.amd){define([],f)}else if(typeof module==='object'&&module.exports){module.exports=f()}else{Object.assign(g,f())}})(this,function(){
+// UMD-style boilerplate for a module that works in both Node.js and browsers.
+(function(r,f){if(typeof module==='object'&&module.exports){module.exports=f()}else{Object.assign(r,f())}})(this,function(){  // eslint-disable-line
 
-// Import dependencies from standard.js.
-var {Matcher, PExp, Terminal, RuleApplication, Choice, Sequence, Repetition, Lookahead, Not} =
-    typeof module === 'object' ? require('./standard') : window;
+  // Import dependencies from standard.js.
+  var {PExp, Terminal, Choice, Sequence, Repetition, Lookahead, Not} =
+      typeof module === 'object' ? require('./standard') : window;
 
-class IncrementalMatcher {
-  constructor(rules) {
-    this.rules = rules;
-    this.memoTable = [];
-    this.input = '';
-  }
+  class IncrementalMatcher {
+    constructor(rules) {
+      this.rules = rules;
+      this.memoTable = [];
+      this.input = '';
+    }
 
-  match() {
-    this.pos = 0;
-    this.maxExaminedPos = -1;
-    var cst = new IncRuleApplication('start').eval(this);
-    return this.pos === this.input.length ? cst : null;
-  }
+    match() {
+      this.pos = 0;
+      this.maxExaminedPos = -1;
+      var cst = new IncRuleApplication('start').eval(this);
+      return this.pos === this.input.length ? cst : null;
+    }
 
-  hasMemoizedResult(ruleName) {
-    var col = this.memoTable[this.pos];
-    return col && col.memo.has(ruleName);
-  }
+    hasMemoizedResult(ruleName) {
+      var col = this.memoTable[this.pos];
+      return col && col.memo.has(ruleName);
+    }
 
-  memoizeResult(pos, ruleName, cst) {
-    var col = this.memoTable[pos];
-    if (!col) {
-      col = this.memoTable[pos] = {
-        memo: new Map(),
-        maxExaminedLength: -1
+    memoizeResult(pos, ruleName, cst) {
+      var col = this.memoTable[pos];
+      if (!col) {
+        col = this.memoTable[pos] = {
+          memo: new Map(),
+          maxExaminedLength: -1
+        };
+      }
+      var examinedLength = this.maxExaminedPos - pos + 1;
+      var entry = {
+        cst: cst,
+        examinedLength: examinedLength
       };
+      if (cst !== null) {
+        entry.matchLength = this.pos - pos;
+      }
+      col.memo.set(ruleName, entry);
+      col.maxExaminedLength =
+          Math.max(col.maxExaminedLength, examinedLength);
     }
-    var examinedLength = this.maxExaminedPos - pos + 1;
-    var entry = {
-      cst: cst,
-      examinedLength: examinedLength
-    };
-    if (cst !== null) {
-      entry.matchLength = this.pos - pos;
+
+    useMemoizedResult(ruleName) {
+      var col = this.memoTable[this.pos];
+      var result = col.memo.get(ruleName);
+      this.maxExaminedPos = Math.max(
+          this.maxExaminedPos,
+          this.pos + result.examinedLength - 1);
+      if (result.cst !== null) {
+        this.pos += result.matchLength;
+        return result.cst;
+      } else {
+        return null;
+      }
     }
-    col.memo.set(ruleName, entry);
-    col.maxExaminedLength =
-        Math.max(col.maxExaminedLength, examinedLength);
-  }
 
-  useMemoizedResult(ruleName) {
-    var col = this.memoTable[this.pos];
-    var result = col.memo.get(ruleName);
-    this.maxExaminedPos = Math.max(
-        this.maxExaminedPos,
-        this.pos + result.examinedLength - 1);
-    if (result.cst !== null) {
-      this.pos += result.matchLength;
-      return result.cst;
-    } else {
-      return null;
+    consume(c) {
+      this.maxExaminedPos =
+          Math.max(this.maxExaminedPos, this.pos);
+      if (this.input[this.pos] === c) {
+        this.pos++;
+        return true;
+      } else {
+        return false;
+      }
     }
-  }
 
-  consume(c) {
-    this.maxExaminedPos =
-        Math.max(this.maxExaminedPos, this.pos);
-    if (this.input[this.pos] === c) {
-      this.pos++;
-      return true;
-    } else {
-      return false;
-    }
-  }
+    applyEdit(startPos, endPos, r) {
+      var s = this.input;
+      var m = this.memoTable;
 
-  applyEdit(startPos, endPos, r) {
-    var s = this.input;
-    var m = this.memoTable;
+      // Step 1: Apply edit to the input
+      this.input =
+          s.slice(0, startPos) + r + s.slice(endPos);
 
-    // Step 1: Apply edit to the input
-    this.input =
-        s.slice(0, startPos) + r + s.slice(endPos);
+      // Step 2: Adjust memo table
+      this.memoTable = m.slice(0, startPos).concat(
+          new Array(r.length).fill(null),
+          m.slice(endPos));
 
-    // Step 2: Adjust memo table
-    this.memoTable = m.slice(0, startPos).concat(
-        new Array(r.length).fill(null),
-        m.slice(endPos));
-
-    // Step 3: Invalidate overlapping entries
-    for (var pos = 0; pos < startPos; pos++) {
-      var col = m[pos];
-      if (col != null &&
-          pos + col.maxExaminedLength > startPos) {
-        var newMax = 0;
-        for (var [ruleName, entry] of col.memo) {
-          var examinedLen = entry.examinedLength;
-          if (pos + examinedLen > startPos) {
-            col.memo.delete(ruleName);
-          } else if (examinedLen > newMax) {
-            newMax = examinedLen;
+      // Step 3: Invalidate overlapping entries
+      for (var pos = 0; pos < startPos; pos++) {
+        var col = m[pos];
+        if (col != null &&
+            pos + col.maxExaminedLength > startPos) {
+          var newMax = 0;
+          for (var [ruleName, entry] of col.memo) {
+            var examinedLen = entry.examinedLength;
+            if (pos + examinedLen > startPos) {
+              col.memo.delete(ruleName);
+            } else if (examinedLen > newMax) {
+              newMax = examinedLen;
+            }
           }
+          col.maxExaminedLength = newMax;
         }
-        col.maxExaminedLength = newMax;
       }
     }
   }
-}
 
-class IncRuleApplication extends PExp {
-  constructor(ruleName) {
-    super();
-    this.ruleName = ruleName;
-  }
+  class IncRuleApplication extends PExp {
+    constructor(ruleName) {
+      super();
+      this.ruleName = ruleName;
+    }
 
-  eval(matcher) {
-    var name = this.ruleName;
-    if (matcher.hasMemoizedResult(name)) {
-      return matcher.useMemoizedResult(name);
-    } else {
-      var origPos = matcher.pos;
-      var origMax = matcher.maxExaminedPos;
-      matcher.maxExaminedPos = -1;
-      var cst = matcher.rules[name].eval(matcher);
-      matcher.memoizeResult(origPos, name, cst);
-      matcher.maxExaminedPos = Math.max(
-          matcher.maxExaminedPos,
-          origMax);
-      return cst;
+    eval(matcher) {
+      var name = this.ruleName;
+      if (matcher.hasMemoizedResult(name)) {
+        return matcher.useMemoizedResult(name);
+      } else {
+        var origPos = matcher.pos;
+        var origMax = matcher.maxExaminedPos;
+        matcher.maxExaminedPos = -1;
+        var cst = matcher.rules[name].eval(matcher);
+        matcher.memoizeResult(origPos, name, cst);
+        matcher.maxExaminedPos = Math.max(
+            matcher.maxExaminedPos,
+            origMax);
+        return cst;
+      }
     }
   }
-}
 
-// Export the same API as 'standard'.
-return {
-  // Same as standard
-  PExp, Terminal, RuleApplication, Choice, Sequence, Repetition, Lookahead, Not,
+  // Export the same API as 'standard'.
+  return {
+    // Classes that are unchanged from standard
+    PExp, Terminal, Choice, Sequence, Repetition, Lookahead, Not,
 
-  // Stuff that needs to be overriden with incremental version
-  Matcher: IncrementalMatcher,
-  RuleApplication: IncRuleApplication
-};
-
-});  // end of UMD
+    // Classes that need to be overridden with incremental versions
+    Matcher: IncrementalMatcher,
+    RuleApplication: IncRuleApplication
+  };
+});
--- src/standard.js
+++ src/standard.js
@@ -1,187 +1,182 @@
 'use strict';
 
-/*
-  Boilerplate for a module that works both in Node.js and in the browser.
-  For Node, `require` this file to get a single object containing all the exports.
-  In the browser, use `<script src="...">` and the exports will be assigned into `window`.
- */
-(function(r,f) {if (typeof module==='object' && module.exports) {module.exports =f()}else{Object.assign(r,f())}})(this,function(){
+// UMD-style boilerplate for a module that works in both Node.js and browsers.
+(function(r,f){if(typeof module==='object'&&module.exports){module.exports=f()}else{Object.assign(r,f())}})(this,function(){  // eslint-disable-line
 
-class Matcher {
-  constructor(rules) {
-    this.rules = rules;
-  }
-
-  match(input) {
-    this.input = input;
-    this.pos = 0;
-    this.memoTable = [];
-    var cst = new RuleApplication('start').eval(this);
-    return this.pos === this.input.length ? cst : null;
-  }
-
-  hasMemoizedResult(ruleName) {
-    var col = this.memoTable[this.pos];
-    return col && col.has(ruleName);
-  }
-
-  memoizeResult(pos, ruleName, cst) {
-    var col = this.memoTable[pos];
-    if (!col) {
-      col = this.memoTable[pos] = new Map();
+  class Matcher {
+    constructor(rules) {
+      this.rules = rules;
     }
-    if (cst !== null) {
-      col.set(ruleName, {cst: cst, nextPos: this.pos});
-    } else {
-      col.set(ruleName, {cst: null});
+
+    match(input) {
+      this.input = input;
+      this.pos = 0;
+      this.memoTable = [];
+      var cst = new RuleApplication('start').eval(this);
+      return this.pos === this.input.length ? cst : null;
     }
-  }
 
-  useMemoizedResult(ruleName) {
-    var col = this.memoTable[this.pos];
-    var result = col.get(ruleName);
-    if (result.cst !== null) {
-      this.pos = result.nextPos;
-      return result.cst;
+    hasMemoizedResult(ruleName) {
+      var col = this.memoTable[this.pos];
+      return col && col.has(ruleName);
     }
-    return null;
-  }
 
-  consume(c) {
-    if (this.input[this.pos] === c) {
-      this.pos++;
-      return true;
-    }
-    return false;
-  }
-}
-
-class PExp {
-  eval(matcher) {
-    throw new Error('abstract method');
-  }
-}
-
-class RuleApplication extends PExp {
-  constructor(ruleName) {
-    super();
-    this.ruleName = ruleName;
-  }
-
-  eval(matcher) {
-    var name = this.ruleName;
-    if (matcher.hasMemoizedResult(name)) {
-      return matcher.useMemoizedResult(name);
-    } else {
-      var origPos = matcher.pos;
-      var cst = matcher.rules[name].eval(matcher);
-      matcher.memoizeResult(origPos, name, cst);
-      return cst;
-    }
-  }
-}
-
-class Terminal extends PExp {
-  constructor(str) {
-    super();
-    this.str = str;
-  }
-
-  eval(matcher) {
-    for (var i = 0; i < this.str.length; i++) {
-      if (!matcher.consume(this.str[i])) {
-        return null;
+    memoizeResult(pos, ruleName, cst) {
+      var col = this.memoTable[pos];
+      if (!col) {
+        col = this.memoTable[pos] = new Map();
+      }
+      if (cst !== null) {
+        col.set(ruleName, {cst: cst, nextPos: this.pos});
+      } else {
+        col.set(ruleName, {cst: null});
       }
     }
-    return this.str;
-  }
-}
 
-class Choice extends PExp {
-  constructor(exps) {
-    super();
-    this.exps = exps;
+    useMemoizedResult(ruleName) {
+      var col = this.memoTable[this.pos];
+      var result = col.get(ruleName);
+      if (result.cst !== null) {
+        this.pos = result.nextPos;
+        return result.cst;
+      }
+      return null;
+    }
+
+    consume(c) {
+      if (this.input[this.pos] === c) {
+        this.pos++;
+        return true;
+      }
+      return false;
+    }
   }
 
-  eval(matcher) {
-    var origPos = matcher.pos;
-    for (var i = 0; i < this.exps.length; i++) {
-      matcher.pos = origPos;
-      var cst = this.exps[i].eval(matcher);
-      if (cst !== null) {
+  class PExp {
+    eval(matcher) {
+      throw new Error('abstract method');
+    }
+  }
+
+  class RuleApplication extends PExp {
+    constructor(ruleName) {
+      super();
+      this.ruleName = ruleName;
+    }
+
+    eval(matcher) {
+      var name = this.ruleName;
+      if (matcher.hasMemoizedResult(name)) {
+        return matcher.useMemoizedResult(name);
+      } else {
+        var origPos = matcher.pos;
+        var cst = matcher.rules[name].eval(matcher);
+        matcher.memoizeResult(origPos, name, cst);
         return cst;
       }
     }
-    return null;
-  }
-}
-
-class Sequence extends PExp {
-  constructor(exps) {
-    super();
-    this.exps = exps;
   }
 
-  eval(matcher) {
-    var ans = [];
-    for (var i = 0; i < this.exps.length; i++) {
-      var exp = this.exps[i];
-      var cst = exp.eval(matcher);
-      if (cst === null) {
-        return null;
-      }
-      if (!(exp instanceof Not)) {
-        ans.push(cst);
-      }
+  class Terminal extends PExp {
+    constructor(str) {
+      super();
+      this.str = str;
     }
-    return ans;
-  }
-}
 
-class Lookahead extends PExp {
-  constructor(exp) {
-    super();
-    this.exp = exp;
-  }
-
-  eval(matcher) {
-    var origPos = matcher.pos;
-    var ans = this.exp.eval(matcher);
-    matcher.pos = origPos;
-    return ans;
-  }
-}
-
-class Not extends Lookahead {
-  eval(matcher) {
-    var ans = super.eval(matcher);
-    return ans === null ? true : null;
-  }
-}
-
-class Repetition extends PExp {
-  constructor(exp) {
-    super();
-    this.exp = exp;
+    eval(matcher) {
+      for (var i = 0; i < this.str.length; i++) {
+        if (!matcher.consume(this.str[i])) {
+          return null;
+        }
+      }
+      return this.str;
+    }
   }
 
-  eval(matcher) {
-    var ans = [];
-    while (true) {
+  class Choice extends PExp {
+    constructor(exps) {
+      super();
+      this.exps = exps;
+    }
+
+    eval(matcher) {
       var origPos = matcher.pos;
-      var cst = this.exp.eval(matcher);
-      if (cst === null) {
+      for (var i = 0; i < this.exps.length; i++) {
         matcher.pos = origPos;
-        break;
-      } else {
-        ans.push(cst);
+        var cst = this.exps[i].eval(matcher);
+        if (cst !== null) {
+          return cst;
+        }
       }
+      return null;
     }
-    return ans;
   }
-}
 
-// Exports
-return {Matcher, PExp, Terminal, RuleApplication, Choice, Sequence, Repetition, Lookahead, Not};
+  class Sequence extends PExp {
+    constructor(exps) {
+      super();
+      this.exps = exps;
+    }
 
-});  // end of UMD
+    eval(matcher) {
+      var ans = [];
+      for (var i = 0; i < this.exps.length; i++) {
+        var exp = this.exps[i];
+        var cst = exp.eval(matcher);
+        if (cst === null) {
+          return null;
+        }
+        if (!(exp instanceof Not)) {
+          ans.push(cst);
+        }
+      }
+      return ans;
+    }
+  }
+
+  class Lookahead extends PExp {
+    constructor(exp) {
+      super();
+      this.exp = exp;
+    }
+
+    eval(matcher) {
+      var origPos = matcher.pos;
+      var ans = this.exp.eval(matcher);
+      matcher.pos = origPos;
+      return ans;
+    }
+  }
+
+  class Not extends Lookahead {
+    eval(matcher) {
+      var ans = super.eval(matcher);
+      return ans === null ? true : null;
+    }
+  }
+
+  class Repetition extends PExp {
+    constructor(exp) {
+      super();
+      this.exp = exp;
+    }
+
+    eval(matcher) {
+      var ans = [];
+      while (true) {
+        var origPos = matcher.pos;
+        var cst = this.exp.eval(matcher);
+        if (cst === null) {
+          matcher.pos = origPos;
+          break;
+        } else {
+          ans.push(cst);
+        }
+      }
+      return ans;
+    }
+  }
+
+  // Exports
+  return {Matcher, PExp, Terminal, RuleApplication, Choice, Sequence, Repetition, Lookahead, Not};
+});
